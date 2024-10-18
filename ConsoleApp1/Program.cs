a6872987596c67388882999009c5dec3cb0ab278
alexeymavrin <a.mavrin@xlab-game.com>
2024-10-18 18:08:19 +0300

    C# Unit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+
+private static void main(string[] args)
+{
+    Console.WriteLine("Hello, World!");
+    
+   
+
+    Console.WriteLine ($" short - sizeof: {sizeof(short)}") min:{short.MinValue} Max:{short.MaxValue};
+    Console.WriteLine ($" ushort - sizeof: {sizeof(ushort)}") min:{short.MinValue} Max:{short.MaxValue};
+    Console.WriteLine ($" byte - sizeof: {sizeof(byte)}");
+    Console.WriteLine ($" bote - sizeof: {sizeof(bote)}");
+    Console.WriteLine ($" int - sizeof: {sizeof(int)}");
+    Console.WriteLine ($" long - sizeof: {sizeof(long)}");
+    
+    
+    
+    
+    
+    
+    
+    }
